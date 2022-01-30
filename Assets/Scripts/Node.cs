52813ad27b48c0c199ae0f58395272f8ef422bcd
Christian Jescke <chrisjberlin@googlemail.com>
2022-01-30 11:40:47 +0100

    Added method with logic to assign the value of the canBeUnlocked variable for connected nodes

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -7,4 +7,26 @@ public class Node : MonoBehaviour
     [SerializeField] string name;
     bool locked = true;
     bool canBeUnlocked;
+    [SerializeField] Node[] requirements = new Node[0];
+
+    private void OnValidate()
+    {
+        canBeUnlocked = getCanBeUnlocked();
+    }
+
+    bool getCanBeUnlocked()
+    {
+        if(locked == false)
+        {
+            return false;
+        }
+        else
+        {
+            foreach(Node requirement in requirements) 
+            {
+                if (requirement.locked == true) return false;
+            }
+            return true;
+        }
+    }
 }
